7322248652c69c1582b1f8499b214aa447bfa03c
Devdeepakjha <dj145740@gmail.com>
2025-08-07 21:07:03 +0530

    Fix: corrected port config for Railway

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 //My Temporary DataBase...
